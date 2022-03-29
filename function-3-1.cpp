089a333d3681851575837ecf8a6528cef2f448ba
TryfanEvans <59408836+TryfanEvans@users.noreply.github.com>
2022-03-29 04:11:02 +0000

    question 3

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -13,8 +13,20 @@ int *readNumbers()
 
 void printNumbers(int *numbers, int length)
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < length; i++)
     {
         std::cout << i << " " << numbers[i] << std::endl;
     }
+}
+
+bool equalsArray(int *numbers1, int *numbers2, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (numbers1[i] != numbers2[i])
+        {
+            return false;
+        }
+    }
+    return true;
 }
