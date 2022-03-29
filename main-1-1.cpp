c5b2a5e52e91afc23ab3e7589b83bc1d22211634
TryfanEvans <59408836+TryfanEvans@users.noreply.github.com>
2022-03-29 03:05:16 +0000

    first question

---
+++ main-1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+extern int *readNumbers();
+extern void printNumbers(int *numbers, int length);
+
+int main()
+{
+
+    printNumbers(readNumbers(), 10);
+  
+    return 0;
+}
