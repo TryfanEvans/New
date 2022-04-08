42a8a5d64e38d7525f0aeaa7f294cd3345a1121b
TryfanEvans <59408836+TryfanEvans@users.noreply.github.com>
2022-04-08 06:26:00 +0000

    cart

---
+++ cart.cpp
@@ -0,0 +1,34 @@
+#include "cart.h"
+#include <iostream>
+
+cart::cart()
+{
+    load = 0;
+} // create an empty cart object
+bool cart::addMeerkat(meerkat cat)
+{
+    if (load < 4)
+    {
+        seats[load] = cat;
+        load++;
+        return true;
+    }
+    return false;
+} // adds a meerkat to the cart, returns false if full
+void cart::emptyCart()
+{
+    load = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        seats[i] = NULL;
+    }
+
+} // remove all meerkats from the cart
+void cart::printMeerkats()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout << seats[i].name << " " << seats[i].age << "\n";
+    }
+} // print the name, a space, the age, then a new line
+  // for each meerkat in the order they were added
---
+++ main-2-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "meerkat.h"
+#include "cart.h"
+
+int main()
+{
+
+    return 0;
+}
