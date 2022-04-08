9efd395765d2eeae71e7b36ba155d30e1ab9dc16
TryfanEvans <59408836+TryfanEvans@users.noreply.github.com>
2022-04-08 06:11:10 +0000

    complete fix

--- meerkat.h
+++ meerkat.h
@@ -9,7 +9,7 @@ private:
 public:
     meerkat();                          // no name or age is required to create a meerkat
     void setName(std::string meerName); // change the meerkat's name
-    string getName();
+    std::string getName();
     void setAge(int meerAge); // change the meerkat's age
     int getAge();
 };
