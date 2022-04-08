986077bbd489e2be954283ec977f37679bdafe23
TryfanEvans <59408836+TryfanEvans@users.noreply.github.com>
2022-04-08 06:46:50 +0000

    error free

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-mymake: meerkat.cpp meerkat.h main-1-1.cpp
-	g++ meerkat.cpp meerkat.h main-1-1.cpp -o out
+mymake: cart.h cart.cpp meerkat.cpp meerkat.h main-2-1.cpp
+	g++ cart.h cart.cpp meerkat.cpp meerkat.h main-2-1.cpp -o out
--- cart.cpp
+++ cart.cpp
@@ -20,7 +20,8 @@ void cart::emptyCart()
     load = 0;
     for (int i = 0; i < 4; i++)
     {
-        seats[i] = NULL;
+        meerkat cat;
+        seats[i] = cat;
     }
 
 } // remove all meerkats from the cart
@@ -28,7 +29,7 @@ void cart::printMeerkats()
 {
     for (int i = 0; i < 4; i++)
     {
-        std::cout << seats[i].name << " " << seats[i].age << "\n";
+        std::cout << seats[i].getName() << " " << seats[i].getAge() << "\n";
     }
 } // print the name, a space, the age, then a new line
   // for each meerkat in the order they were added
