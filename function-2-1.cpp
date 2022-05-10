8e0de18ea9160ac46c9f3af812a1000d0a537c82
TryfanEvans <59408836+TryfanEvans@users.noreply.github.com>
2022-05-10 04:25:46 +0000

    with testing

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -17,3 +17,13 @@ string lookup_spectrum(int day)
                         "Violet"};
     return colors[day - 1];
 }
+
+// repeatedly calls the lookup_spectrum function so tests it for both valid and invalid spectrum numbers and prints the result
+void test()
+{
+    for (int i = 1; i < 8; i++)
+    {
+        cout << lookup_spectrum(i) << endl;
+    }
+    cout << lookup_spectrum(34) << endl;
+}
