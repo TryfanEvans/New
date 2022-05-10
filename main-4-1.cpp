d3807460c36ef4c38eb76e8dc09812c8b76bd6d2
TryfanEvans <59408836+TryfanEvans@users.noreply.github.com>
2022-05-10 05:21:48 +0000

    adding trailing spaces

--- main-4-1.cpp
+++ main-4-1.cpp
@@ -1,13 +1,18 @@
+#include <iostream>
+
 extern int *sparse_count(int **vals, int num_rows, int num_cols);
 
 int main()
 {
-    int row1[] = {10, 15, 0};
-    int row2[] = {0, 5, 73};
-    int row3[] = {0, 0, 1};
+    int row1[] = {10, 15, 0, 3};
+    int row2[] = {0, 5, 73, 3};
+    int row3[] = {0, 0, 1, 3};
     int *vals[] = {row1, row2, row3};
     int nrows = 3;
-    int ncols = 3;
-
+    int ncols = 4;
+    int *mm = sparse_count(vals, nrows, ncols);
+    std::cout << mm[0] << " " << mm[1] << "\n";
+    mm = sparse_count(vals, nrows, ncols);
+    std::cout << mm[0] << " " << mm[1] << "\n";
     return 0;
 }
